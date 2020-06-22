ecb76e2b107ed108764654922d46c82d08de5ea5
MaSch2020 <66020705+MaSch2020@users.noreply.github.com>
2020-06-22 13:08:31 -0400

    Fixed the while loop

--- Ejemplos.java
+++ Ejemplos.java
@@ -3,17 +3,15 @@ public class Ejemplos {
 
 	public Ejemplos() {
 	}
-	
+
 	public void ciclo() {
 		int cont = 1;
-		String mens = "Error";
-		while(cont < 10){
-			System.out.println(mens);
+		String mens = "Error ";
+		while (cont < 11) {
+			System.out.println(mens + cont);
+			cont++;
 		}
 	}
-	
-	
-	
 
 	public static void main(String[] args) {
 
